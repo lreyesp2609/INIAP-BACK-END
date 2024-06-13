48fc0719ecaa43d43619ebb255e7c3709c558027
lreyesp2609 <lreyesp@uteq.edu.ec>
2024-06-13 14:18:07 -0500

    Arreglo en la bd + modelos del backend

---
+++ bd.sql
@@ -0,0 +1,245 @@
+CREATE TABLE Provincias (
+    id_provincia SERIAL PRIMARY KEY,
+    Provincia VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Ciudades (
+    id_ciudad SERIAL PRIMARY KEY,
+    id_provincia INT NOT NULL,
+    Ciudad VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id_provincia) REFERENCES Provincias(id_provincia)
+);
+
+CREATE TABLE Rol (
+    id_rol SERIAL PRIMARY KEY,
+    rol VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(255)
+);
+
+CREATE TABLE Personas (
+    id_persona SERIAL PRIMARY KEY,
+    numero_cedula VARCHAR(20) NOT NULL,
+    nombres VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    fecha_nacimiento DATE,
+    genero VARCHAR(20),
+    celular VARCHAR(20),
+    direccion VARCHAR(255),
+    correo_electronico VARCHAR(100)
+);
+
+CREATE TABLE Usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    id_rol INT NOT NULL,
+    id_persona INT NOT NULL,
+    usuario VARCHAR(50) NOT NULL,
+    contrasenia VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_rol) REFERENCES Rol(id_rol),
+    FOREIGN KEY (id_persona) REFERENCES Personas(id_persona)
+);
+
+CREATE TABLE Estaciones (
+    id_estacion SERIAL PRIMARY KEY,
+    nombre_estacion VARCHAR(100) NOT NULL,
+    siglas_estacion VARCHAR(20),
+    ruc VARCHAR(20),
+    direccion VARCHAR(255),
+    telefono VARCHAR(20)
+);
+
+CREATE TABLE Unidades (
+    id_unidad SERIAL PRIMARY KEY,
+    nombre_unidad VARCHAR(100) NOT NULL,
+    siglas_unidad VARCHAR(20),
+    id_estacion INT NOT NULL,
+    FOREIGN KEY (id_estacion) REFERENCES Estaciones(id_estacion)
+);
+
+
+CREATE TABLE Cargos (
+    id_cargo SERIAL PRIMARY KEY,
+    id_unidad INT NOT NULL,
+    cargo VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id_unidad) REFERENCES Unidades(id_unidad)
+);
+
+
+CREATE TABLE Empleados (
+    id_empleado SERIAL PRIMARY KEY,
+    id_persona INT NOT NULL,
+    id_cargo INT,
+    distinto VARCHAR(100),
+    fecha_ingreso DATE,
+    habilitado SMALLINT DEFAULT 1,
+    FOREIGN KEY (id_persona) REFERENCES Personas(id_persona),
+    FOREIGN KEY (id_cargo) REFERENCES Cargos(id_cargo)
+);
+
+CREATE TABLE Tipo_Licencias (
+    id_tipo_licencia SERIAL PRIMARY KEY,
+    tipo_licencia VARCHAR(50) NOT NULL,
+    observacion VARCHAR(255)
+);
+
+CREATE TABLE Bancos (
+    id_banco SERIAL PRIMARY KEY,
+    nombre_banco VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Cuentas_Bancarias (
+    id_cuenta_bancaria SERIAL PRIMARY KEY,
+    id_banco INT NOT NULL,
+    id_empleado INT NOT NULL,
+    tipo_cuenta VARCHAR(50),
+    numero_cuenta VARCHAR(50),
+    habilitado SMALLINT DEFAULT 1,
+    FOREIGN KEY (id_banco) REFERENCES Bancos(id_banco),
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado)
+);
+
+CREATE TABLE Empleados_Tipo_Licencias (
+    id_empleado INT NOT NULL,
+    id_tipo_licencia INT NOT NULL,
+    PRIMARY KEY (id_empleado, id_tipo_licencia),
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado),
+    FOREIGN KEY (id_tipo_licencia) REFERENCES Tipo_Licencias(id_tipo_licencia)
+);
+
+CREATE TABLE Ordenes_Movilizacion (
+    id_orden_movilizacion SERIAL PRIMARY KEY,
+    secuencial_orden_movilizacion VARCHAR(50) NOT NULL,
+    fecha_hora_emision TIMESTAMP NOT NULL,
+    fecha_desde DATE,
+    hora_desde TIME,
+    fecha_hasta DATE,
+    hora_hasta TIME,
+    habilitado SMALLINT DEFAULT 1
+);
+
+CREATE TABLE Tipo_Ordenes_Movilizacion (
+    id_tipo_om SERIAL PRIMARY KEY,
+    tipo_orden_movilizacion VARCHAR(100) NOT NULL,
+    nomenclatura VARCHAR(20),
+    abreviatura VARCHAR(10)
+);
+
+CREATE TABLE Solicitudes (
+    id_solicitud SERIAL PRIMARY KEY,
+    fecha_solicitud DATE NOT NULL,
+    motivo_movilizacion VARCHAR(255),
+    fecha_salida_solicitud DATE,
+    hora_salida_solicitud TIME,
+    fecha_llegada_solicitud DATE,
+    hora_llegada_solicitud TIME,
+    descripcion_actividades TEXT,
+    listado_empleado TEXT,
+    estado_solicitud VARCHAR(50),
+    id_empleado INT NOT NULL,
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado)
+);
+
+CREATE TABLE Transporte_Solicitudes (
+    id_transporte_soli SERIAL PRIMARY KEY,
+    id_solicitud INT NOT NULL,
+    tipo_transporte_soli VARCHAR(50),
+    ruta_soli VARCHAR(255),
+    fecha_salida_soli DATE,
+    hora_salida_soli TIME,
+    fecha_llegada_soli DATE,
+    hora_llegada_soli TIME,
+    FOREIGN KEY (id_solicitud) REFERENCES Solicitudes(id_solicitud)
+);
+
+CREATE TABLE Informes (
+    id_informes SERIAL PRIMARY KEY,
+    id_solicitud INT NOT NULL,
+    secuencia_informe INT,
+    fecha_informe DATE,
+    fecha_salida_informe DATE,
+    hora_salida_informe TIME,
+    fecha_llegada_informe DATE,
+    hora_llegada_informe TIME,
+    evento VARCHAR(255),
+    observacion TEXT,
+    FOREIGN KEY (id_solicitud) REFERENCES Solicitudes(id_solicitud)
+);
+
+CREATE TABLE Actividades_Informes (
+    id_informe INT NOT NULL,
+    dia DATE,
+    actividad VARCHAR(255),
+    PRIMARY KEY (id_informe),
+    FOREIGN KEY (id_informe) REFERENCES Informes(id_informes)
+);
+
+CREATE TABLE Facturas_Informes (
+    id_informe INT NOT NULL,
+    tipo_documento VARCHAR(50),
+    fecha_emision DATE,
+    detalle_documento VARCHAR(255),
+    valor DECIMAL(10, 2),
+    PRIMARY KEY (id_informe, tipo_documento),
+    FOREIGN KEY (id_informe) REFERENCES Informes(id_informes)
+);
+
+CREATE TABLE Productos_Alcanzados_Informes (
+    id_producto_alcanzado SERIAL PRIMARY KEY,
+    id_informe INT NOT NULL,
+    descripcion TEXT,
+    FOREIGN KEY (id_informe) REFERENCES Informes(id_informes)
+);
+
+CREATE TABLE Transporte_Informe (
+    id_transporte_informe SERIAL PRIMARY KEY,
+    id_informe INT NOT NULL,
+    tipo_transporte_info VARCHAR(50),
+    nombre_transporte_info VARCHAR(100),
+    ruta_info VARCHAR(255),
+    fecha_salida_info DATE,
+    hora_salida_info TIME,
+    fecha_llegada_info DATE,
+    hora_llegada_info TIME,
+    FOREIGN KEY (id_informe) REFERENCES Informes(id_informes)
+);
+
+CREATE TABLE Categorias_Bienes (
+    id_categorias_bien SERIAL PRIMARY KEY,
+    descripcion_categoria VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Subcategorias_Bienes (
+    id_subcategoria_bien SERIAL PRIMARY KEY,
+    id_categorias_bien INT NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    identificador VARCHAR(50),
+    FOREIGN KEY (id_categorias_bien) REFERENCES Categorias_Bienes(id_categorias_bien)
+);
+
+CREATE TABLE Vehiculo (
+    id_vehiculo SERIAL PRIMARY KEY,
+    id_subcategoria_bien INT NOT NULL,
+    placa VARCHAR(20) NOT NULL,
+    codigo_inventario VARCHAR(50),
+    modelo VARCHAR(50),
+    marca VARCHAR(50),
+    color_primario VARCHAR(50),
+    color_secundario VARCHAR(50),
+    anio_fabricacion INTEGER,
+    numero_motor VARCHAR(50),
+    numero_chasis VARCHAR(50),
+    numero_matricula VARCHAR(50),
+    habilitado SMALLINT DEFAULT 1,
+    FOREIGN KEY (id_subcategoria_bien) REFERENCES Subcategorias_Bienes(id_subcategoria_bien)
+);
+
+CREATE TABLE control_ordenes_movilizacion (
+    id_com SERIAL PRIMARY KEY,
+    ultima_numeracion INT NOT NULL
+);
+
+CREATE TABLE control_solicitudes (
+    id_control_solicitudes SERIAL PRIMARY KEY,
+    anio INT NOT NULL,
+    ultima_secuencia INT NOT NULL,
+    fecha_ultima_secuencia DATE NOT NULL
+);
---
+++ inserción.sql
@@ -0,0 +1,109 @@
+BEGIN;
+-- Insertar estación
+INSERT INTO Estaciones (nombre_estacion, siglas_estacion, ruc, direccion, telefono)
+VALUES
+  (
+    'Estación Experimental Tropical Pichilingue',  -- Nombre de la estación
+    'EETP',                                       -- Siglas de la estación
+    '1',                                          -- RUC
+    'Vía a El Empalme, Quevedo',                  -- Dirección
+    '(05) 278-3044'                               -- Teléfono
+  );
+
+-- Insertar unidad
+INSERT INTO Unidades (nombre_unidad, siglas_unidad, id_estacion)
+VALUES
+  (
+    'UNIDAD DE INFORMATICA',  -- Nombre de la unidad
+    'UI',                     -- Siglas de la unidad
+    (SELECT id_estacion FROM Estaciones WHERE nombre_estacion = 'Estación Experimental Tropical Pichilingue')  -- id_estacion
+  );
+
+-- Insertar cargo
+INSERT INTO Cargos (id_unidad, cargo)
+VALUES
+  (
+    (SELECT id_unidad FROM Unidades WHERE nombre_unidad = 'UNIDAD DE INFORMATICA'),  -- id_unidad
+    'ASISTENTE TIC 1 DE ESTACION EXPERIMENTAL'                                      -- Cargo
+  );
+
+-- Inserta usuario para admin
+INSERT INTO auth_user (
+    password,
+    last_login,
+    is_superuser,
+    username,
+    first_name,
+    last_name,
+    email,
+    is_staff,
+    is_active,
+    date_joined
+) VALUES (
+    'pbkdf2_sha256$720000$nA1DBiCSy5A4HPTMAfMGDx$MmlhOsKKop+XKgi48HY1WYVShwtU6vD1/nsc8bnk2Zo=', -- Reemplaza con la contraseña cifrada
+    NULL,              -- last_login, puedes dejarlo como NULL
+    true,              -- is_superuser, puede ser true o false según tus necesidades
+    'admin1',
+    'luis',
+    'reyes',
+    'correo@example.com',
+    true,              -- is_staff, puede ser true o false según tus necesidades
+    true,              -- is_active, puede ser true o false según tus necesidades
+    current_timestamp   -- date_joined, utiliza la fecha y hora actual
+);
+
+-- Insertar roles
+INSERT INTO Rol (rol, descripcion)
+VALUES
+  ('Administrador', 'Rol con acceso completo al sistema'),
+  ('Empleado', 'Rol para usuarios regulares'),
+  ('Supervisor', 'Rol para supervisores de unidades');
+
+-- Insertar persona
+INSERT INTO Personas (numero_cedula, nombres, apellidos, fecha_nacimiento, genero, celular, direccion, correo_electronico)
+VALUES
+  (
+    '0123456789',       -- Número de cédula
+    'Luis',             -- Nombres
+    'Reyes',            -- Apellidos
+    '2002-09-26',       -- Fecha de nacimiento (ejemplo)
+    'Masculino',        -- Género
+    '0991234567',       -- Celular
+    'Av. Principal 123',-- Dirección
+    'luis@example.com'  -- Correo electrónico
+  );
+
+-- Obtener el id_persona recién insertado
+DO $$
+DECLARE
+    persona_id INT;
+    cargo_id INT;
+BEGIN
+    -- Obtener el id_persona recién insertado
+    SELECT id_persona INTO persona_id FROM Personas WHERE numero_cedula = '0123456789';
+
+    -- Obtener el id_cargo de 'ASISTENTE TIC 1 DE ESTACION EXPERIMENTAL'
+    SELECT id_cargo INTO cargo_id FROM Cargos WHERE cargo = 'ASISTENTE TIC 1 DE ESTACION EXPERIMENTAL';
+
+    -- Insertar usuario Empleado
+    INSERT INTO Usuarios (id_rol, id_persona, usuario, contrasenia)
+    VALUES
+    (
+        (SELECT id_rol FROM Rol WHERE rol = 'Empleado'),  -- Reemplaza con el ID del rol 'Empleado'
+        persona_id,  -- id_persona recién obtenido
+        'luis.reyes',  -- Usuario
+        'pbkdf2_sha256$720000$nA1DBiCSy5A4HPTMAfMGDx$MmlhOsKKop+XKgi48HY1WYVShwtU6vD1/nsc8bnk2Zo='  -- Contraseña cifrada
+    );
+
+    -- Insertar en Empleados
+    INSERT INTO Empleados (id_persona, id_cargo, fecha_ingreso, habilitado)
+    VALUES
+    (
+        persona_id,  -- id_persona recién obtenido
+        cargo_id,    -- id_cargo recién obtenido
+        current_date, -- Fecha de ingreso actual
+        1            -- Habilitado (1 para true)
+    );
+END $$;
+
+COMMIT;
