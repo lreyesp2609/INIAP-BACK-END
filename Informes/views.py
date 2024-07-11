ec8381380b625907bca48e6bcccc3d64d7df4354
lreyes2609 <lreyesp@uteq.edu.ec>
2024-07-11 09:50:11 -0500

    Permite crear ordenes

--- Informes/views.py
+++ Informes/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 import jwt
 
 from .models import Empleados, Personas, Unidades, Estaciones, Solicitudes, Informes, Usuarios
-from datetime import datetime
+from datetime import datetime, date
 import json
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -26,7 +26,7 @@ from django.db.models import Max
 
 from django.conf import settings
 import jwt
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import F, Func, Value, CharField
 from django.views import View
 from django.http import JsonResponse
@@ -43,7 +43,7 @@ class CrearSolicitudView(View):
         else:
             nueva_secuencia_numero = 1
 
-        return f'{nueva_secuencia_numero:03}'
+        return nueva_secuencia_numero
 
     def post(self, request, id_usuario, *args, **kwargs):
         try:
@@ -51,15 +51,22 @@ class CrearSolicitudView(View):
             usuario = Usuarios.objects.get(id_usuario=id_usuario)
             empleado = Empleados.objects.get(id_persona=usuario.id_persona)
 
-            # Obtener datos del formulario FormData
-            fecha_solicitud = request.POST.get('fecha_solicitud')
-            motivo_movilizacion = request.POST.get('motivo_movilizacion', '')
-            fecha_salida_solicitud = request.POST.get('fecha_salida_solicitud', '')
-            hora_salida_solicitud = request.POST.get('hora_salida_solicitud', '')
-            fecha_llegada_solicitud = request.POST.get('fecha_llegada_solicitud', '')
-            hora_llegada_solicitud = request.POST.get('hora_llegada_solicitud', '')
-            descripcion_actividades = request.POST.get('descripcion_actividades', '')
-            listado_empleado = request.POST.get('listado_empleado', '')
+            # Obtener datos del JSON enviado en el cuerpo del request
+            data = json.loads(request.body)
+            motivo_movilizacion = data.get('motivo_movilizacion', '')
+            fecha_salida_solicitud = data.get('fecha_salida_solicitud', '')
+            hora_salida_solicitud = data.get('hora_salida_solicitud', '')
+            fecha_llegada_solicitud = data.get('fecha_llegada_solicitud', '')
+            hora_llegada_solicitud = data.get('hora_llegada_solicitud', '')
+            descripcion_actividades = data.get('descripcion_actividades', '')
+            listado_empleado = data.get('listado_empleado', '')
+
+            # Convertir las fechas de texto a objetos date si es necesario
+            if fecha_salida_solicitud:
+                fecha_salida_solicitud = datetime.strptime(fecha_salida_solicitud, '%Y-%m-%d').date()
+
+            if fecha_llegada_solicitud:
+                fecha_llegada_solicitud = datetime.strptime(fecha_llegada_solicitud, '%Y-%m-%d').date()
 
             # Generar secuencia_solicitud
             secuencia_solicitud = self.incrementar_secuencia_solicitud(empleado)
@@ -68,7 +75,7 @@ class CrearSolicitudView(View):
             with transaction.atomic():
                 solicitud = Solicitudes.objects.create(
                     secuencia_solicitud=secuencia_solicitud,
-                    fecha_solicitud=fecha_solicitud,
+                    fecha_solicitud=date.today(),
                     motivo_movilizacion=motivo_movilizacion,
                     fecha_salida_solicitud=fecha_salida_solicitud,
                     hora_salida_solicitud=hora_salida_solicitud,
@@ -82,16 +89,13 @@ class CrearSolicitudView(View):
 
             return JsonResponse({'mensaje': 'Solicitud creada exitosamente', 'id_solicitud': solicitud.id_solicitud}, status=201)
 
-        except Usuarios.DoesNotExist:
-            return JsonResponse({'error': 'El usuario no existe'}, status=404)
-
-        except Empleados.DoesNotExist:
-            return JsonResponse({'error': 'El empleado correspondiente al usuario no existe'}, status=404)
+        except ObjectDoesNotExist:
+            return JsonResponse({'error': 'El usuario o el empleado no existe'}, status=404)
 
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
 
-
+        
 class ListarSolicitudesView(View):
     def get(self, request, id_usuario, *args, **kwargs):
         try:
