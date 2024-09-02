50648e02c2db6ed14d9fa9c1e83a65a6bc7615d9
Bryan Eduardo Robalino Chuez <eduardochuez_2003@outlook.com>
2024-09-01 23:40:05 -0500

    editar cargos

--- Cargos/urls.py
+++ Cargos/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('cargoseditar/', ListaCargosView2.as_view(), name='lista_cargos'),
     path('crear-cargos/<int:id_usuario>/<int:id_unidad>/', CrearCargoView.as_view(), name='crear_cargo'),
     path('cargos-unidad/<int:id_usuario>/<int:id_unidad>/', ListaCargosPorUnidadView.as_view(), name='lista_cargos_por_unidad'),
-
+    path('editar-cargos/<int:id_usuario>/<int:id_cargo>/', EditarCargoView.as_view(), name='editar-cargos'),
 ]
